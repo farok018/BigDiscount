842059a0fa5c30ca5e6b22a22489655f90cb825c
hairul <hairul@localhost>
2023-02-02 23:22:25 -0600

    i am uploading my first customer list

---
+++ src/customerlist/international.java
@@ -0,0 +1,11 @@
+package customerlist;
+
+public class international {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to DevOps Training");
+		System.out.println("by Farok");
+		System.out.println(20+50);
+	}
+
+}
